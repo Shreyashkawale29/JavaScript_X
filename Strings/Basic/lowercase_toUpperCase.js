3d559c66e4d0a2048904cb35518265496d15574e
Shreyash Kawale <kawalesn01@gmail.com>
2025-11-26 14:52:36 +0530

    Lowercase_toUpperCase.js

---
+++ Strings/Basic/lowercase_toUpperCase.js
@@ -0,0 +1,20 @@
+// Given a string str containing only lowercase letters, generate a string with the same letters, but in uppercase.
+
+// Example 1:
+
+// Input:
+let str = "geeks";
+// Output: GEEKS
+// Example 2:
+
+// Input:
+// str = "geeksforgeeks"
+// Output: GEEKSFORGEEKS
+
+function lowercase_toUpperCase(str) {
+  let result = str.toUpperCase();
+
+  return console.log(result);
+}
+
+lowercase_toUpperCase(str);
